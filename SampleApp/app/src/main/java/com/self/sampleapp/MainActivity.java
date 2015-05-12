b8e6a65e42493b6010402e46dfff3ae3eaac2a4c
Raghav Chaitanya Petluru <chaitanyaraghav@gmail.com>
2015-05-12 00:25:32 -0400

    testing git check-in

--- SampleApp/app/src/main/java/com/self/sampleapp/MainActivity.java
+++ SampleApp/app/src/main/java/com/self/sampleapp/MainActivity.java
@@ -28,14 +28,7 @@ public class MainActivity extends ActionBarActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
-        if (id == android.R.id.home){
-            return true;
-        }
 
         return super.onOptionsItemSelected(item);
     }
